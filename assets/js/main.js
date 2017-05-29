9a5dc2b0c31b156f749c3fbb7fcc5979371bff0e
YessiGarcia <aria.801900@gmail.com>
2017-05-29 16:58:21 -0500

    botones funcionales

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,62 @@
-$(document).ready (function(){
+/*$(document).ready (function(){
     $('#carrusel div:gt(0)').hide();
     setInterval(function(){
       $('#carrusel div:first-child').fadeOut(0)
          .next('div').fadeIn(1000)
-         .end().appendTo('#carrusel');}, 4000);
+         .end().appendTo('#carrusel');}, 1000);
+});*/
+
+$(document).ready(function (){
+    $(".img").hide();
+    $("#00").show();
+});
+
+$("#0").click(function(){
+    $(".img").hide();
+    $("#00").show();
+});
+
+$("#1").click(function(){
+    $(".img").hide();
+    $("#01").show();
+});
+
+$("#2").click(function(){
+    $(".img").hide();
+    $("#02").show();
+});
+
+$("#3").click(function(){
+    $(".img").hide();
+    $("#03").show();
+});
+
+$("#4").click(function(){
+    $(".img").hide();
+    $("#04").show();
+});
+
+$("#5").click(function(){
+    $(".img").hide();
+    $("#05").show();
+});
+
+$("#6").click(function(){
+    $(".img").hide();
+    $("#06").show();
+});
+
+$("#7").click(function(){
+    $(".img").hide();
+    $("#07").show();
+});
+
+$("#8").click(function(){
+    $(".img").hide();
+    $("#08").show();
+});
+
+$("#9").click(function(){
+    $(".img").hide();
+    $("#09").show();
 });
